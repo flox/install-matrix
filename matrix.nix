b2fd6c843ce3abaced4184ef8c732fa15c329fb9
Matthew Kenigsberg <matthewkenigsberg@gmail.com>
2022-05-27 22:16:12 -0500

    Basic test suite for flox deb and rpm

--- matrix.nix
+++ matrix.nix
@@ -12,14 +12,106 @@ let
     '';
   };
 
-  testScripts = {
-    hello = ''
-      #!/bin/sh
-      echo hello world
+  testScripts = let
+    verify_installed_common = ''
+      # TODO finish these criteria and make sure they're all tested
+      # installed means:
+      #   scriptlets ensure:
+      #   - the installer closure is in the /nix/store and added to the nix database
+      #   - systemd units are enabled and nix-daemon.socket is started
+      #   - there is a zero length file at /usr/share/nix/nix.tar.xz
+      #   - users and groups are added
+      #   - selinux policy is loaded
+      #   - TODO verify this: channels are set up
+      #   - TODO anything else in the tarball from /nix?
+      #   the package manager tracks all of the following files:
+      #   - there is a gcroot for each derivation used to create the closure in FLOX_GCROOT, and there are no additional gcroots in FLOX_GCROOT
+      #   - binaries are linked in /usr/bin and /usr/sbin
+      #   - man pages are linked in /usr/share
+      #   - files from the rootfs directory
+      #   - systemd unit files
+      #   - various other files: flox.toml, repo files, nix-daemon.conf, flox-version
+      #
+      # State machine:
+      # uninstalled -install_1-> installed_1
+      # installed_1 -uninstall_1-> uninstalled
+      #             -upgrade-> installed_2
+      # TODO add transitions for installed_2
+      # if we run install, uninstall, install, upgrade, and uninstall we'll hit a good chunk of this
+
+      systemctl status nix-daemon.socket
+      # nix-daemon.service must be started by the socket otherwise permissions won't be correct
+      systemctl status nix-daemon.service && exit 1
+      sudo nix-collect-garbage -d
+      # uncomment after this doesn't run through interactive flox setup
+      #[[ $(flox --version) =~ "Version: "[0-9]\.[0-9]\.[0-9] ]]
+      man flox > /dev/null
+      # nix can do something non-trivial. This is kinda slow because it pulls nixpkgs...
+      [[ $(nix run nixpkgs#hello) == "Hello, world!" ]]
+      man nix > /dev/null
+      systemctl status nix-daemon.socket
+      systemctl status nix-daemon.service
+      test -f /etc/systemd/system/sockets.target.wants/nix-daemon.socket
+      test -f /etc/systemd/system/multi-user.target.wants/nix-daemon.service
     '';
-    version = ''
-      #!/bin/sh
-      echo --version
+    verify_installed_fedora = ''
+      ${verify_installed_common}
+      sudo semodule --list | grep nix > /dev/null
+      dnf search flox --repo flox | grep "Exactly Matched"
+      sudo dnf update flox --repo flox
+    '';
+    verify_installed_ubuntu = ''
+      ${verify_installed_common}
+      sudo apt-get update -o Dir::Etc::sourcelist="/etc/apt/sources.list.d/flox.list" -o Dir::Etc::sourceparts="-"
+    '';
+    verify_uninstalled = ''
+      # uninstalled means (for now focus on things scriptlets do, since they're more likely to have mistakes):
+      #   - /nix does not exist
+      #   - systemd units are disabled
+      test -d /nix && exit 1
+      systemctl status nix-daemon.socket && exit 1
+      systemctl status nix-daemon.service && exit 1
+      test -f /etc/systemd/system/sockets.target.wants/nix-daemon.socket && exit 1
+      test -f /etc/systemd/system/multi-user.target.wants/nix-daemon.service && exit 1
+      grep nixbld /etc/passwd && exit 1
+      getent group nixbld && exit 1
+      test -f /usr/bin/nix && exit 1
+    '';
+    verify_uninstalled_fedora = ''
+      ${verify_uninstalled}
+      sudo semodule --list | grep nix && exit 1
+      dnf repolist --repo flox && exit 1
+    '';
+  in {
+    test-rpm = ''
+      set -euxo pipefail
+      ${verify_installed_fedora}
+      floxPath1=$(readlink /usr/bin/flox)
+      sudo rpm --erase flox
+      ${verify_uninstalled_fedora}
+      sudo rpm -i flox.rpm
+      ${verify_installed_fedora}
+      sudo rpm -U flox2.rpm
+      [[ $floxPath1 != $(readlink /usr/bin/flox) ]]
+      ${verify_installed_fedora}
+      sudo rpm --erase flox
+      ${verify_uninstalled_fedora}
+      exit 0
+    '';
+    test-deb = ''
+      set -euxo pipefail
+      ${verify_installed_ubuntu}
+      floxPath1=$(readlink /usr/bin/flox)
+      sudo dpkg --purge flox
+      ${verify_uninstalled}
+      sudo dpkg --install flox.deb
+      ${verify_installed_ubuntu}
+      sudo dpkg --install flox2.deb
+      [[ $floxPath1 != $(readlink /usr/bin/flox) ]]
+      ${verify_installed_ubuntu}
+      sudo dpkg --purge flox
+      ${verify_uninstalled}
+      exit 0
     '';
   };
 
@@ -31,8 +123,10 @@ let
   };
 
   filters = {
-    imageFilter = "alpine-3-14";
-    #testFilter = "hello";
+    installFilter = null;
+    imageFilter = null;
+    loginFilter = "login";
+    testFilter = "test-rpm";
   };
 
   "alpine-default" = {
